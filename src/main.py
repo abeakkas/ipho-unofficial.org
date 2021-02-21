d953b7e103c36099b5f2fe40d4cf852451518b11
Abdurrahman Akkas <abdurrahmanakkas@gmail.com>
2021-02-20 16:16:34 -0800

    Remove php backwards compatibility files.
    It's been several years since we changed the system to static files. Hopefully there are no links out there anymore that rely on these files.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
+
 import index
 import e404
 import timeline
 import countries
 import search
 import static_files
-import backward_compatibility
 
 def run():
     print("Creating whole project")
@@ -15,7 +15,6 @@ def run():
     countries.run()
     search.run()
     static_files.run()
-    backward_compatibility.run()
-    
+
 if __name__ == "__main__":
     run()
