e57f5f53567d21276c8355704896e43ea8c7dbbf
Abdurrahman Akkas <abdurrahmanakkas@gmail.com>
2021-07-24 20:36:53 +0300

    Increase config years

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ webmaster_email = "iphounofficial@gmail.com"
 
 # Technically, these can be non-consecutive
 # and wow that actually happened in 2020.
-previous_year = "2019"
-next_year = "2021"
+previous_year = "2021"
+next_year = "2022"
 
 # On github, the links don't have to have .html suffix,
 # and directories forward to index.html inside.
