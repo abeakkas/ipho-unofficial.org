5a7738f9f9cdf833f3327d4cdd63c244523761e0
Abdurrahman Akkas <abdurrahmanakkas@gmail.com>
2022-06-24 22:18:53 -0700

    404 to the root

--- src/e404.py
+++ src/e404.py
@@ -5,7 +5,9 @@ def run():
     print("Creating 404")
     html = templates.get("404")
     html = templates.initial_replace(html, -1)
-    html = templates.final_replace(html, "..")
+    # This can't both work local and Github :/
+    # but it only works on Github anyways
+    html = templates.final_replace(html, "")
     util.writefile("../404.html", html)
 
 if __name__ == "__main__":
