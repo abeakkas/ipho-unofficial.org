91f37cba030e958f2b0a4bdf267ae99e87a17ba5
Abdurrahman Akkas <abdurrahmanakkas@gmail.com>
2022-06-24 22:11:22 -0700

    Better 404 from subfolders

--- src/e404.py
+++ src/e404.py
@@ -5,7 +5,7 @@ def run():
     print("Creating 404")
     html = templates.get("404")
     html = templates.initial_replace(html, -1)
-    html = templates.final_replace(html, ".")
+    html = templates.final_replace(html, "..")
     util.writefile("../404.html", html)
 
 if __name__ == "__main__":
