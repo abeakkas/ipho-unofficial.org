2bbe961c4f16739d2057a2757154fec8285673ae
Abdurrahman Akkas <abdurrahmanakkas@gmail.com>
2024-08-04 22:11:03 -0700

    Increment year

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ webmaster_email = "iphounofficial@gmail.com"
 
 # Technically, these can be non-consecutive
 # and wow that actually happened in 2020.
-previous_year = "2023"
-next_year = "2024"
+previous_year = "2024"
+next_year = "2025"
 
 # On github, the links don't have to have .html suffix,
 # and directories forward to index.html inside.
