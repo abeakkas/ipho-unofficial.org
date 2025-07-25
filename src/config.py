b9ec7ee331285bd40825a449ad0480528334b7a9
Abdurrahman Akkas <abdurrahmanakkas@gmail.com>
2025-07-25 10:26:36 -0700

    Bump year, 2024 minutes

--- src/config.py
+++ src/config.py
@@ -2,10 +2,10 @@ webmaster_email = "iphounofficial@gmail.com"
 
 # Technically, these can be non-consecutive
 # and wow that actually happened in 2020.
-previous_year = "2024"
-next_year = "2025"
+previous_year = "2025"
+next_year = "2026"
 
-# On github, the links don't have to have .html suffix,
-# and directories forward to index.html inside.
+# On GitHub, the URLs can omit the .html suffix and directories forward to
+# index.html inside. URLs are simplifies when enabled.
 github = True
 
