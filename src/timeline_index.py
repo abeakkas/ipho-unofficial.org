55ab8bdc2b0ee4fa6e54aba2e62e641b2b7aafdc
Abdurrahman Akkas <abdurrahmanakkas@gmail.com>
2026-07-25 18:48:35 -0700

    timeline_index refactor

--- src/timeline_index.py
+++ src/timeline_index.py
@@ -18,7 +18,7 @@ def run():
   tablehtml = ""
   upcominghtml = ""
   upcoming_row_ctr = 0
-  for edition in editions:
+  for edition in reversed(editions):
     if edition.host2:
       code2 = edition.host2.code
       country2 = edition.host2.name
@@ -44,23 +44,26 @@ def run():
     )
 
     if edition.year <= next_year + 2:
-      # Reverse list
-      tablehtml = rowhtml + tablehtml
+      # IPhO 2020 was a special event and is not listed in timeline database.
+      if edition.year == 2019:
+        tablehtml += render_fragment("timeline/index_row_2020")
+      tablehtml += rowhtml
     else:
-      upcominghtml = rowhtml + upcominghtml
+      upcominghtml += rowhtml
       upcoming_row_ctr += 1
-    # IPhO 2020 was a special event and is not listed in timeline database.
-    if edition.year == 2019:
-      tablehtml = render_fragment("timeline/index_row_2020") + tablehtml
 
   # Append an empty row to preserve row parity between tables for styling purposes
   if upcoming_row_ctr % 2:
     upcominghtml = "<tr style=\"display:none;\"></tr>" + upcominghtml
 
+  # Hide the "Upcoming Years" button when there are no upcoming editions to reveal.
+  upcoming_cue_style = "" if upcoming_row_ctr else "display:none"
+
   render_page(
     "timeline/index",
     table=tablehtml,
     upcoming=upcominghtml,
+    upcoming_cue_style=upcoming_cue_style,
   )
 
 if __name__ == "__main__":
